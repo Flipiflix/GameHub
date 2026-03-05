d725c98d39486888844f62b87556257364322254
Flipiflix <felix.priebisch@gmail.com>
2026-03-05 15:06:27 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use('/gamehub', express.static('public'));
+
+app.use('/', createProxyMiddleware({
+  target: 'https://www.mcdle.net',
+  changeOrigin: true,
+  selfHandleResponse: false,
+  on: {
+    proxyRes: (proxyRes) => {
+      delete proxyRes.headers['x-frame-options'];
+      delete proxyRes.headers['content-security-policy'];
+      proxyRes.headers['access-control-allow-origin'] = '*';
+    }
+  }
+}));
+
+app.listen(PORT, () => {
+  console.log('Server läuft auf Port ' + PORT);
+});
