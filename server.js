32e052672b2ea0715662a90f5300d5d5c47e8d0d
Flipiflix <felix.priebisch@gmail.com>
2026-03-06 12:18:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,20 +6,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use('/gamehub', express.static('public'));
 
-app.use('/flagle', createProxyMiddleware({
-  target: 'https://www.flagle-game.com/unlimited',
-  changeOrigin: true,
-  pathRewrite: { '^/flagle': '' },
-  selfHandleResponse: false,
-  on: {
-    proxyRes: (proxyRes) => {
-      delete proxyRes.headers['x-frame-options'];
-      delete proxyRes.headers['content-security-policy'];
-      proxyRes.headers['access-control-allow-origin'] = '*';
-    }
-  }
-}));
-
 app.use('/', createProxyMiddleware({
   target: 'https://www.mcdle.net',
   changeOrigin: true,
