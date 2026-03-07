f31a7e8eb74b7dc54e6e162493e7308a0b08599c
Flipiflix <felix.priebisch@gmail.com>
2026-03-07 01:27:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ app.use('/gamehub', express.static('public'));
 app.use('/flagle', createProxyMiddleware({
   target: 'https://www.flagle-game.com',
   changeOrigin: true,
-  pathRewrite: { '^/flagle': '' },
   selfHandleResponse: false,
-  autoRewrite: true,
-  cookieDomainRewrite: '',
   onproxyRes: (proxyRes) => {
     delete proxyRes.headers['x-frame-options'];
     delete proxyRes.headers['content-security-policy'];
