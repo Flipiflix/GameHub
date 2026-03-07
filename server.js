fc34f5ef6c5b82be96509d98e0f8e6a30a8c79a6
Flipiflix <felix.priebisch@gmail.com>
2026-03-07 01:08:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,48 +11,20 @@ app.use('/flagle', createProxyMiddleware({
   target: 'https://www.flagle-game.com',
   changeOrigin: true,
   pathRewrite: { '^/flagle': '' },
-  selfHandleResponse: true,
+  selfHandleResponse: false,
+  autoRewrite: true,
+  cookieDomainRewrite: '',
   on: {
-    proxyRes: (proxyRes, req, res) => {
+    proxyRes: (proxyRes) => {
       delete proxyRes.headers['x-frame-options'];
       delete proxyRes.headers['content-security-policy'];
       proxyRes.headers['access-control-allow-origin'] = '*';
 
       if (proxyRes.headers['location']) {
         proxyRes.headers['location'] = proxyRes.headers['location']
-          .replace('https://www.flagle-game.com', '/flagle');
+          .replace('https://www.flagle-game.com', '')
+          .replace('http://www.flagle-game.com', '');
       }
-
-      const contentType = proxyRes.headers['content-type'] || '';
-      const encoding = proxyRes.headers['content-encoding'];
-
-      if (!contentType.includes('text/html')) {
-        res.writeHead(proxyRes.statusCode, proxyRes.headers);
-        proxyRes.pipe(res);
-        return;
-      }
-
-      let chunks = [];
-      proxyRes.on('data', chunk => chunks.push(chunk));
-      proxyRes.on('end', () => {
-        const buf = Buffer.concat(chunks);
-        const decode = (data) => {
-          let html = data.toString('utf8');
-          html = html.replace(/<div[^>]*cookie[^>]*>[\s\S]*?<\/div>/gi, '');
-          html = html.replace(/<div[^>]*consent[^>]*>[\s\S]*?<\/div>/gi, '');
-          html = html.replace(/<script[^>]*>[\s\S]*?(cookie|consent|gdpr)[\s\S]*?<\/script>/gi, '');
-          delete proxyRes.headers['content-encoding'];
-          res.writeHead(proxyRes.statusCode, proxyRes.headers);
-          res.end(html);
-        };
-        if (encoding === 'gzip') {
-          zlib.gunzip(buf, (err, d) => decode(err ? buf : d));
-        } else if (encoding === 'br') {
-          zlib.brotliDecompress(buf, (err, d) => decode(err ? buf : d));
-        } else {
-          decode(buf);
-        }
-      });
     }
   }
 }));
